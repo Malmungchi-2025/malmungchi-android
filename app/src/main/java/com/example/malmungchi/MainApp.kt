b509302b7b708a8b2f2b1463d311be8ae5e2fccf
Chea-yunzi <katete051@naver.com>
2025-09-23 17:15:52 +0900

    ui 오류 수정

--- app/src/main/java/com/example/malmungchi/MainApp.kt
+++ app/src/main/java/com/example/malmungchi/MainApp.kt
@@ -86,6 +86,7 @@ import com.malmungchi.feature.login.LevelReadingQuizRoute
 import com.malmungchi.feature.login.LevelTestIntroRoute
 import com.malmungchi.feature.login.LevelsViewModel
 import com.malmungchi.feature.mypage.MyPageViewModel
+import com.malmungchi.feature.mypage.badge.BadgeCollectionRoute
 import com.malmungchi.feature.mypage.nickname.NicknameCardDialog
 //import com.malmungchi.feature.mypage.nickname.NicknameCardScreen
 import com.malmungchi.feature.quiz.QuizCategoryRoute
